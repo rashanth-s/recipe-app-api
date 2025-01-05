5014ff8fcfa3bdb1a4a7a3d8d932da0949556ba2
rashanth-s <rashanthsgowda@gmail.com>
2025-01-05 17:07:33 +0530

    Postge SQL installation

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample test cases for the app module.
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_sub(self):
+        res = calc.sub(5, 6)
+
+        self.assertEqual(res, -1)
+        
