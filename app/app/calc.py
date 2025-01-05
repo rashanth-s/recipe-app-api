abb641e9aa0a0c1130c4f14d9dd670032961e388
rashanth-s <rashanthsgowda@gmail.com>
2025-01-05 17:12:19 +0530

    Postge SQL installation - Linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,13 @@
 Calculation functions
 """
 
+
 def add(x, y):
     """Add two numbers together"""
     return x + y
 
+
 def sub(x, y):
     """Subtract x from y and return value"""
     return x-y
+    
